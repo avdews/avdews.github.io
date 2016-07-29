49471bfca89fbc14e8876c79fb2fdff752a5c8ef
Avery Dews <abdews2@gmail.com>
2016-07-29 02:22:46 -0400

    resolved merge conflict

---
+++ js/sendMessage.js
@@ -0,0 +1,13 @@
+var message = "";
+
+$("sendMessage").on("click", function() {
+	message = $("#contactform").serialize();
+	$.ajax({
+		url: "//formspree.io/avdews@umich.edu",
+		method: "POST",
+		data: {message: message},
+		dataType: "json"
+	});
+	alert('Thanks for the message, I\'ll be in touch soon.');
+	return false;
+});
