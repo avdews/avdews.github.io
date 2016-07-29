63b47c1c04359cafd3bed6b3aee443cacc3df6ec
Avery Dews <abdews2@gmail.com>
2016-07-29 00:14:48 -0400

    update contact form

--- js/contactform.js
+++ js/contactform.js
@@ -1,13 +1,15 @@
-var message = "";
+function sendMessage() {
+	var message = "";
 
-$("contact-button").on("click", function() {
-	message = $("#contactform").serialize();
-	$.ajax({
-		url: "//formspree.io/avdews@umich.edu",
-		method: "POST",
-		data: {message: message},
-		dataType: "json"
+	$("contact-button").on("click", function() {
+		message = $("#contactform").serialize();
+		$.ajax({
+			url: "//formspree.io/avdews@umich.edu",
+			method: "POST",
+			data: {message: message},
+			dataType: "json"
+		});
+		alert('Thanks for the message, I\'ll be in touch soon.');
+		return false;
 	});
-	alert('Thanks for the message, I\'ll be in touch soon.');
-	return false;
-});
+}
